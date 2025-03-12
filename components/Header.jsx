3208ecb8242beb96f8616e86de2a33c465df7376
vivek <kandarivivek260@gmail.com>
2025-03-12 13:28:57 +0530

    updated version

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@
 
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="header">
         <div className="header-content">
             <h2 className="title"><a href="/">Where in the world?</a></h2>
             <p className="theme-chnager"><i className="fa-regular fa-moon"></i>&nbsp;&nbsp;Dark Mode</p>
---
+++ components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SearchBar() {
+  return (
+    <div className="search-container" bis_skin_checked="1">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Search for a Country...."/>
+      </div>
+  )
+}
